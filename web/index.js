6e4cc606c1dc9b24d215a056d6e1635a6db0324d
amodinho <u14284783@tuks.co.za>
2019-06-20 12:35:21 +0200

    changed capitilationzation

--- web/index.js
+++ web/index.js
@@ -4,6 +4,6 @@ const sls = require('serverless-http')
 const binaryMimeTypes = require('./binaryMimeTypes')
 
 const server = require('./server')
-module.exports.server = sls(Server, {
+module.exports.server = sls(server, {
     binary: binaryMimeTypes
 })

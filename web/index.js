fc08a6b78606cb3d948ddd47164e73a160fa6247
amodinho <u14284783@tuks.co.za>
2019-06-27 15:29:22 +0200

    Deleted other nonsesne and added new plugin

--- web/index.js
+++
@@ -1,9 +0,0 @@
-// index.js entry for our handler?
-
-const sls = require('serverless-http')
-const binaryMimeTypes = require('./binaryMimeTypes')
-
-const server = require('./server')
-module.exports.server = sls(server, {
-    binary: binaryMimeTypes
-})

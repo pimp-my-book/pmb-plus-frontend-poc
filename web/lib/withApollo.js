eac42cc0d749a8e82418c0d47270bb01084fecf8
amodinho <u14284783@tuks.co.za>
2019-07-05 13:07:20 +0200

    Set up with apollo

---
+++ web/lib/withApollo.js
@@ -0,0 +1,21 @@
+/*
+This is the withApollo wrapper that allows to connect 
+client to backend.
+
+*/
+
+import withApollo from 'next-with-apollo';
+import ApolloClient from 'apollo-boost';
+
+function createClient({headers}) {
+    return new ApolloClient({
+        uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+        headers
+    });
+
+
+
+
+}
+
+export default withApollo(createClient);

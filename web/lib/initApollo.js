cc3a8a12a663d13c205b499744703cdf9696c578
amodinho <u14284783@tuks.co.za>
2019-07-05 15:18:04 +0200

    Done with the create

--- web/lib/initApollo.js
+++ web/lib/initApollo.js
@@ -1,3 +1,20 @@
 // This is the initApollo file where we will connect to the backend
 import {ApolloClient, InMemoryCache, HttpLink} from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
+
+let ApolloClient = null
+
+function create (initialState){
+    const isBrowser = typeof window !== 'undefined'
+    return new ApolloClient({
+        connectToDevTools: isBrowser,
+        ssrMode: !isBrowser,
+        link: new HttpLink({
+            uri: 'http://localhost:4000/graphql',
+            credentials: 'same-origin',
+            fetch: !isBrowser && fetch
+        }),
+        cache: new InMemoryCache().restore(initialState || {})
+    })
+}
+

60be44ab594d7b90a7e751a2b3898834a7e9d66a
amodinho <u14284783@tuks.co.za>
2019-07-05 14:56:49 +0200

    Changed the value of the endpoint

--- web/lib/withApollo.js
+++ web/lib/withApollo.js
@@ -6,7 +6,7 @@ client to backend.
 
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
-
+import {endpoint, devEndpoint} from '../config';
 function createClient({headers}) {
     return new ApolloClient({
         uri: process.env.NODE_ENV === 'development' ? endpoint : devEndpoint,

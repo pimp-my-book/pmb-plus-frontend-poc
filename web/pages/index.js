f3336356bc841b63c48eee23d31e0f2ec27458d9
amodinho <u14284783@tuks.co.za>
2019-07-05 16:28:22 +0200

    Trying get the api to displays tuff

--- web/pages/index.js
+++ web/pages/index.js
@@ -2,13 +2,34 @@
 //index.js
 import App from './App'
 import withApollo from '../lib/withApollo'
+import {Query} from 'react-apollo'
+import gql from 'graphql-tag'
+
+
+export const HelloQuery = gql`
+  query HELLO {
+      hello
+  }
+`
+
 const Home = () => {
-    return(
-       <App>
-       <h1>Hello Ya'll</h1>
-       </App>
+    return (
+        <App>
+        <Query query={HelloQuery}>
+         {({loading, error, data}) =>{
+             if (loading) return <p>loading</p>;
+             if (error) return <p>{error}</p>;
+             return(
+                
+                <h1>{data}</h1>
+               
+             )
+         }}
+        </Query>
+         </App>
     )
+    
     
 }
 
-export default withApollo(Home);
+export default Home;

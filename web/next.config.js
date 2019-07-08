7869ad3a116bdb5d02e356bcff0a3be40a39a9a0
amodinho <u14284783@tuks.co.za>
2019-07-08 14:46:10 +0200

    Setting up stages

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,18 @@
-module.exports = {
+import {PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD} from 'next/constants'
+
+
+module.exports = phase => {
+    //get the dev stage
+    const isDev = phase = PHASE_DEVELOPMENT_SERVER
+
+    //get the prod stage
+    const isProd = phase = PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+
+
+    
     serverRuntimeConfig: {
         localEndpoint:'http://localhost:4000/graphql',
-        devEndpoint:'https://a9mmxv28mh.execute-api.us-east-1.amazonaws.com/dev/graphql'
+        devEndpoint: 'https://a9mmxv28mh.execute-api.us-east-1.amazonaws.com/dev/graphql'
 
     }
 }

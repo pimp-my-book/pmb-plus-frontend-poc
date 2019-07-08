d842da9dc6ba94abb5e1ac940667cc3b1afe4478
amodinho <u14284783@tuks.co.za>
2019-07-08 14:02:09 +0200

    add stage variables

--- web/lib/initApollo.js
+++ web/lib/initApollo.js
@@ -2,6 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost'
 import fetch from 'isomorphic-unfetch'
 
 let apolloClient = null
+const stage = process.env.REACT_APP_STAGE === "prod";
 
 function create (initialState) {
   // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient

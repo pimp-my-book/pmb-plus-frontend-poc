42bcf474ddc9b95001a621c82cc2441618bca112
amodinho <u14284783@tuks.co.za>
2019-07-08 14:21:12 +0200

    add urls to next config

---
+++ web/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    serverRuntimeConfig: {
+        localEndpoint:'http://localhost:4000/graphql',
+        devEndpoint:'https://a9mmxv28mh.execute-api.us-east-1.amazonaws.com/dev/graphql'
+
+    }
+}

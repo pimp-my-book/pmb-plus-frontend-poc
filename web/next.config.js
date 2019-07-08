0fc5b1ad2a2312ac79adbf69cb30722d3aee14c6
amodinho <u14284783@tuks.co.za>
2019-07-08 14:50:00 +0200

    Finished the config

--- web/next.config.js
+++ web/next.config.js
@@ -7,12 +7,20 @@ module.exports = phase => {
 
     //get the prod stage
     const isProd = phase = PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
-
-
-    
-    serverRuntimeConfig: {
-        localEndpoint:'http://localhost:4000/graphql',
-        devEndpoint: 'https://a9mmxv28mh.execute-api.us-east-1.amazonaws.com/dev/graphql'
-
+ 
+    //make an env object to return the right URL based on the stage
+    const env = {
+        API_ENDPOINTS: (() => {
+            if (isDev) return 'http://localhost:4000/graphql'
+            if (isProd) {
+                return 'https://a9mmxv28mh.execute-api.us-east-1.amazonaws.com/dev/graphql'
+            }
+        })
     }
+
+    //return an env object for the config
+    return {
+        env
+    }
+
 }

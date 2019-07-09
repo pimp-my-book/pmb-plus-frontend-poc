a7421d185bb18d00e71f5ed848bea489bd95e19a
amodinho <u14284783@tuks.co.za>
2019-07-09 16:58:06 +0200

    Added the colours names

---
+++ web/components/Page.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import styled, {ThemeProvider, injectGlobal} from 'styled-components'
+
+
+const theme = {
+    pinkDarkest: '',
+    pinkDark: '',
+    pink: '',
+    pinkLight: '',
+    pinkLighest:'',
+    black:'',
+    greyDark: '',
+    grey:'',
+    greyLight: '',
+    greyLightest: '',
+    white: '',
+    greenDarkest: '',
+    greenDark: '',
+    green: '',
+    greenLight: '',
+    greenLightest: '',
+    blueDarkest: '',
+    blueDark: '',
+    blue: '',
+    blueLight: '',
+    blueLightest: '',
+    redDarkest: '',
+    redDark: '',
+    red: '',
+    redLight: '',
+    redLightest: '',
+    orangeDarkest: '',
+    orangeDark:'',
+    orange: '',
+    orangeLight: '',
+    orangeLightest: '',
+    
+
+}

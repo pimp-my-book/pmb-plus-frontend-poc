c75b4e3b63a991e38cf5e67a074367c41e06e1f2
amodinho <u14284783@tuks.co.za>
2019-07-09 17:26:37 +0200

    Done with the page

--- web/components/Page.js
+++ web/components/Page.js
@@ -37,3 +37,38 @@ const theme = {
 
 
 }
+
+
+
+injectGlobal`
+@import url('https://fonts.googleapis.com/css?family=Kanit|Rubik+Mono+One&display=swap');
+
+       h1,h2,h3{
+        font-family: 'Rubik Mono One', sans-serif;
+       }
+
+       p { 
+        font-family: 'Kanit', sans-serif;
+       }
+
+        body {
+            margin: 0;
+        }
+
+`
+
+class Page extends Component {
+    render(){
+        return (
+            <ThemeProvider theme={theme}>
+             <StyledPage>
+                  <Meta/>
+                  <Header/>
+                  <Inner>{this.props.children}</Inner>
+             </StyledPage>
+            </ThemeProvider>
+        )
+    }
+}
+
+export default Page

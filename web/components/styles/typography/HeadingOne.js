3bbda2c0d33a3566ab275669113ced9bf3369d8b
amodinho <u14284783@tuks.co.za>
2019-07-10 12:51:00 +0200

    Removed font

--- web/components/styles/typography/HeadingOne.js
+++ web/components/styles/typography/HeadingOne.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 
-const HeadingOne = styled.h2`
+const HeadingOne = styled.h1`
   font-size: 44px;
   line-height: 100%;
   font-weight: 500;
   color: #ffc4d0;
-  font-family: 'Paytone One', sans-serif;
 `
 
 export default HeadingOne

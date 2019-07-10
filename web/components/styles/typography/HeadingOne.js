5b498865028604ba5115bb3c2cd5674cf3114201
amodinho <u14284783@tuks.co.za>
2019-07-10 12:50:20 +0200

    made heading one

---
+++ web/components/styles/typography/HeadingOne.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const HeadingOne = styled.h2`
+  font-size: 44px;
+  line-height: 100%;
+  font-weight: 500;
+  color: #ffc4d0;
+  font-family: 'Paytone One', sans-serif;
+`
+
+export default HeadingOne

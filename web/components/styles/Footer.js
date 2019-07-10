520c23584c5380a202a8808febf6193d0c6536fd
amodinho <u14284783@tuks.co.za>
2019-07-10 11:56:42 +0200

    Done with the footer

--- web/components/styles/Footer.js
+++ web/components/styles/Footer.js
@@ -2,8 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 import {Container, Row, Col} from 'react-bootstrap'
 
-const Footer = () => 
+const FooterStyles = styled.footer`
+height: 100px;
+width: 100%;
+background-color: ${props => props.theme.blueDarkest};
+border-bottom: ${props => props.theme.pinkDarkest} 8px solid;
+`
 
+const Footer = () => 
+<FooterStyles>
+    
+</FooterStyles>
 
 
 export default Footer

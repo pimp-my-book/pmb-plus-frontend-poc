e094a905f57a4762f59a6a0f0b119c5df8b8f1a4
amodinho <u14284783@tuks.co.za>
2019-07-10 12:58:16 +0200

    Added temp styles

--- web/components/styles/buttons/PrimaryButton.js
+++ web/components/styles/buttons/PrimaryButton.js
@@ -1,2 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+
+
+const PrimaryButtonStyles = styled.button`
+font-family: 'Poppins', sans-serif;
+color: var(--rasin-black);
+background-color: var(--rose-pink);
+border: ${props => props.border ? '2px solid var(--rose-pink)' : 'none'} ;
+width: ${props => props.small ? '100px' : '180px'};
+font-size: 20px;
+padding: 2px;
+:hover {
+ background-color: var(--rose-pink);
+ color: var(--linen);
+   }
+   border: 1px solid var(--rose-pink);
+}
+
+
+`

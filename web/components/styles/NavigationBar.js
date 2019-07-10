9419b2b5a36ed8e72a29ca64caca1f14b0774b21
amodinho <u14284783@tuks.co.za>
2019-07-10 11:00:26 +0200

    Done with navBar

--- web/components/styles/NavigationBar.js
+++ web/components/styles/NavigationBar.js
@@ -1,3 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
 import {NavBar} from 'react-bootstrap'
+
+const NavbarStyles = styled(NavBar)`
+&&&{
+    background-color: ${props => props.theme.blueDarkest};
+    border-top: ${props => props.theme.pinkDarkest} 8px solid;
+}
+
+`
+
+
+const NavigationBar = ({
+    expand,
+    ...props
+}) => {
+    return(
+        <NavbarStyles
+        expand="lg"
+        {...props}>
+        </NavbarStyles>
+    )
+}
+
+export default NavigationBar

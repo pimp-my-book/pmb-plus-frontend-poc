c226dd66d1f96604d5f86ee6c0a2bdfc4bfb46d2
amodinho <u14284783@tuks.co.za>
2019-07-10 12:54:52 +0200

    Created a body text component

---
+++ web/components/styles/typography/BodyText.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const BodyText = styled.p`
+  font-size: 16px;
+  line-height: 100%;
+  font-weight: 500;
+  color: ${props => props.theme.black};
+`
+
+export default BodyText

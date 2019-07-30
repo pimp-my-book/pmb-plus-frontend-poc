f0b7577a7f072bb98c08b037d017eef45052667d
amodinho <u14284783@tuks.co.za>
2019-07-30 16:44:10 +0200

    added padding

--- web/components/styles/cards/BookCard.js
+++ web/components/styles/cards/BookCard.js
@@ -8,6 +8,7 @@ import HeadingThree from "../typography/HeadingThree"
 const BookCardStyles = styled.div`
 height: 450px;
 width:330px;
+padding: 10px;
 background-color: ${props => props.theme.white};
 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 border-radius: 5px;

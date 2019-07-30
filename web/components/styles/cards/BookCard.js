d1cad0007ce953eced073d2a5a8aa26f8aff9a9c
amodinho <u14284783@tuks.co.za>
2019-07-30 15:15:01 +0200

    added book card

---
+++ web/components/styles/cards/BookCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const BookCardStyles = styled.div`
+
+`
+
+const BookCard = ({
+    text
+}) => {
+    return(
+        <BookCardStyles>
+
+        </BookCardStyles>
+    )
+}
+
+export default BookCard

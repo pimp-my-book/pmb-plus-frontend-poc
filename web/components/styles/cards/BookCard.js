91f7f05f9e9a90bb67ff553f5ed10b8c988e210f
amodinho <u14284783@tuks.co.za>
2019-07-30 15:33:02 +0200

    adding props

--- web/components/styles/cards/BookCard.js
+++ web/components/styles/cards/BookCard.js
@@ -2,11 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const BookCardStyles = styled.div`
-
+height: 450px;
+width:330px;
+background-color: ${props => props.theme.white};
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 5px;
 `
 
 const BookCard = ({
-    text
+    text,
+    cardImg,
+    bookTitle,
+    bookPrice,
+    bookGrade,
 }) => {
     return(
         <BookCardStyles>

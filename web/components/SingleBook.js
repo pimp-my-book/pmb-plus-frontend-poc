81455ef4ed589361b7829291e65bd9d76f49ca07
amodinho <u14284783@tuks.co.za>
2019-07-31 14:55:35 +0200

    done configuring book view

--- web/components/SingleBook.js
+++ web/components/SingleBook.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react'
 import {GET_ONE_BOOK} from '../graphql/Queries'
+import {Query} from 'react-apollo'
 
 export default class SingleBook extends Component{
     render(){
         return(
-            <div>
-                hi
-            </div>
+            <Query
+            query={GET_ONE_BOOK}
+            variables={{
+                ID: this.props.ID
+            }}>
+            {({error, loading, data}) =>{
+                if(error) return <p>Something Went wrong</p>;
+                if(loading) return <p>Loading...</p>;
+                if (!data) return <p>No Book found for this item</p>;
+                const book = data.getOneBook
+                return (
+                    <div>
+                    {book.title}
+                    {book.vendor}
+                    {book.price}
+                    {book.image}
+
+                    {book.author}
+                    {book.ISBN}
+                    {book.edition}
+                    {book.grade}
+                    </div>
+                    
+                )
+            }}
+            </Query>
         )
     }
 }

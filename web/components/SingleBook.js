aea5aec79db9ea555b6571f5cc993b49d019fcee
amodinho <u14284783@tuks.co.za>
2019-07-31 14:45:53 +0200

    added single book component

---
+++ web/components/SingleBook.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import {GET_ONE_BOOK} from '../graphql/Queries'
+
+export default class SingleBook extends Component{
+    render(){
+        return(
+            <div>
+                hi
+            </div>
+        )
+    }
+}
